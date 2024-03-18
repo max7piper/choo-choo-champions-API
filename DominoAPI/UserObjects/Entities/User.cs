86eb0292bd67185193c4ca85213da2597c272dcc
max7piper <112013316+max7piper@users.noreply.github.com>
2024-03-17 19:34:53 -0500

    Updated To Match Figma

--- DominoAPI/UserObjects/Entities/User.cs
+++ DominoAPI/UserObjects/Entities/User.cs
@@ -15,9 +15,17 @@ public class User
 
     public string Password { get; set; } = string.Empty;
 
-    public int Wins { get; set;} = 0;
+    public int TotalGameWins { get; set;} = 0;
 
-    public int Losses { get; set;} = 0;
+    public int TotalRoundWins { get; set;} = 0;
 
-    public double AverageScore {get; set;} = 0.0;
+    public double AveragePointsPerGame {get; set;} = 0.0;
+
+    public double AveragePointsPerRound {get; set;} = 0.0;
+
+    public int TotalPoints {get; set;} = 0;
+
+    public int WinRanking {get; set;} = 0;
+
+    public int PointRanking {get; set;} = 0;
 }
