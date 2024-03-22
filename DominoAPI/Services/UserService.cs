628134827113c63d2b16f8498e07bde3b2d9f229
Arjun R <arjun.ramesh16@gmail.com>
2024-03-22 12:33:53 -0500

    Get endpoint for profile image

--- DominoAPI/Services/UserService.cs
+++ DominoAPI/Services/UserService.cs
@@ -9,28 +9,34 @@ public class UserService
 {
     private readonly IMongoCollection<User> _userCollection;
 
-    public UserService(
-        IOptions<UserDatabaseSettings> userDatabaseSettings)
+    public UserService(IOptions<UserDatabaseSettings> userDatabaseSettings)
     {
-        var mongoClient = new MongoClient(
-            userDatabaseSettings.Value.ConnectionString);
+        var mongoClient = new MongoClient(userDatabaseSettings.Value.ConnectionString);
 
-        var mongoDatabase = mongoClient.GetDatabase(
-            userDatabaseSettings.Value.DatabaseName);
+        var mongoDatabase = mongoClient.GetDatabase(userDatabaseSettings.Value.DatabaseName);
 
         _userCollection = mongoDatabase.GetCollection<User>(
-            userDatabaseSettings.Value.UserCollectionName);
+            userDatabaseSettings.Value.UserCollectionName
+        );
     }
 
-    public async Task<List<User>> GetAsync() =>
-        await _userCollection.Find(_ => true).ToListAsync();
+    public async Task<List<User>> GetAsync() => await _userCollection.Find(_ => true).ToListAsync();
 
     public async Task<User> GetByUsername(String username) =>
         await _userCollection.Find(u => u.Username.Equals(username)).FirstOrDefaultAsync();
 
-    public async Task CreateAsync(User newUser) =>
-        await _userCollection.InsertOneAsync(newUser);
+    public async Task CreateAsync(User newUser) => await _userCollection.InsertOneAsync(newUser);
 
     public async Task UpdateAsync(string username, User updatedUser) =>
         await _userCollection.ReplaceOneAsync(x => x.Username == username, updatedUser);
+
+    public async Task<string> GetProfileImage(string username)
+    {
+        User user = await _userCollection.Find(u => u.Username == username).FirstOrDefaultAsync();
+        if (user != null)
+        {
+            return user.ImageLink;
+        }
+        return null;
+    }
 }
--- DominoAPI/UserRepositories/IUserRepository.cs
+++ DominoAPI/UserRepositories/IUserRepository.cs
@@ -7,26 +7,33 @@ namespace DominoAPI.UserRepositories
     /// </summary>
     public interface IUserRepository
     {
-         /// <summary>
-         /// Registers a user into the database
-         /// </summary>
-         /// <param name="user">The user object we are registering into the database.</param>
-         /// <returns></returns>
-         public Task<User> registerUser(User user);
+        /// <summary>
+        /// Registers a user into the database
+        /// </summary>
+        /// <param name="user">The user object we are registering into the database.</param>
+        /// <returns></returns>
+        public Task<User> registerUser(User user);
 
-         /// <summary>
-         /// Logs a user in to the game
-         /// </summary>
-         /// <param name="username">The users username</param>
-         /// <param name="password">The users password, unhashed</param>
-         /// <returns></returns>
-         public Task<User> login(String username, String password);
+        /// <summary>
+        /// Logs a user in to the game
+        /// </summary>
+        /// <param name="username">The users username</param>
+        /// <param name="password">The users password, unhashed</param>
+        /// <returns></returns>
+        public Task<User> login(String username, String password);
 
         /// <summary>
         /// Updates a user's profile image.
         /// </summary>
         /// <param name="username">The user that needs to be updated.</param>
         /// <returns></returns>
-         public Task<User> updateProfileImage(String username, String imageURL);
+        public Task<User> updateProfileImage(String username, String imageURL);
+
+        /// <summary>
+        /// Retrieves the profile image URL associated with the given username.
+        /// </summary>
+        /// <param name="username">The username of the user whose profile image URL is to be retrieved.</param>
+        /// <returns>The profile image URL, or null if not found.</returns>
+        Task<string> GetProfileImage(string username);
     }
 }
